1eec2d6efbb48c16813dba09e228c0d276de0492
ModestRSA <56158279+ModestRSA@users.noreply.github.com>
2019-10-27 21:29:25 +0200

    HouseKeeping
    -Moved all temp classes into its own folder
    - started declaring varibles in GameMap Class

--- Assets/Scripts/GameMap.cs
+++ Assets/Scripts/GameMap.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class GameMap : MonoBehaviour
 {
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public enum TileTypeTeam1
+    {
+        MeleeUnit_Team1,
+        RangedUnit_Team1,
+        WizzardUnit_Team1,
+        Resource_Team1,
+        Factory_Team1
+    }
+    public enum TileTypeTeam2
+    {
+        MeleeUnit_Team2,
+        RangedUnit_Team2,
+        WizzardUnit_Team2,
+        Resource_Team2,
+        Factory_Team2
+    }
+    public enum Neutral
+    {
+        Wizzard_Neutral,
+        OpenTile,
+        Wall
+    }
+
     // Start is called before the first frame update
     void Start()
     {
