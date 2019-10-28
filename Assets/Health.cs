cbf493c170033a5077db72d0d8b1d68199c8694d
ModestRSA <56158279+ModestRSA@users.noreply.github.com>
2019-10-28 03:50:03 +0200

    Health Bars
    Added Health Bars to units*
    * only some units are displaying them.

---
+++ Assets/Health.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int MaxHealth = 100;
+
+    private int currentHealth;
+
+    public event Action<float> OnHealthPctChanged = delegate { };
+    public void ModifyHealth(int amount)
+    {
+        currentHealth += MaxHealth;
+        float currentHealthPct = (float)currentHealth / (float)MaxHealth;
+        OnHealthPctChanged(currentHealthPct);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ModifyHealth(-10);
+        }
+    }
+}
---
+++ Assets/HealthBar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    private Image foreGroundImage;
+    [SerializeField]
+    private float updateSpeedSeconds = 0.5f;
+
+    private void Awake()
+    {
+        GetComponentInParent<Health>().OnHealthPctChanged += HandleHealthChanged;
+    }
+    private void HandleHealthChanged(float pct)
+    {
+        StartCoroutine(ChangeToPct(pct));
+    }
+    private IEnumerator ChangeToPct(float pct)
+    {
+        float preChacngePct = foreGroundImage.fillAmount;
+        float elapsed = 0f;
+
+        while(elapsed<updateSpeedSeconds)
+        {
+            elapsed += Time.deltaTime;
+            foreGroundImage.fillAmount = Mathf.Lerp(preChacngePct, pct, elapsed / updateSpeedSeconds);
+            yield return null;
+        }
+        foreGroundImage.fillAmount = pct;
+    }
+    private void LateUpdate()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(0, 180, 0);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
