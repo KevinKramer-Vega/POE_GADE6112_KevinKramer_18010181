763425d98101de4634a332ed3360fd1b95cfab46
ModestRSA <56158279+ModestRSA@users.noreply.github.com>
2019-10-28 02:25:45 +0200

    Camera controller update
    Updated the camera controller to use a mouse click to rotate the camera

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -7,6 +7,19 @@ public class cameraController : MonoBehaviour
     private float moveSpeed = 0.5f;
     private float scrollSpeed = 10f;
 
+    //Rotation Varibles
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
+
+    public float sensX = 100.0f;
+    public float sensY = 100.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +38,23 @@ public class cameraController : MonoBehaviour
         {
             transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
         }
+        if (Input.GetMouseButton(0))
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        //float rotation = 0;
+        //if(Input.GetKey(KeyCode.Q))
+        //{
+        //    rotation -= -1;
+        //}
+        //if(Input.GetKey(KeyCode.E))
+        //{
+        //    rotation += 1;
+        //}
+        //transform.Rotate(0, rotation, 0);
+
     }
 }
